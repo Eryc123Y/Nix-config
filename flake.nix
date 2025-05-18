eca4ec085e285ed228ca709afe7e1b0a425d2304
Eryc123Y <ericyxy123@gmail.com>
2025-05-18 19:10:46 +0800

    update flake

--- flake.nix
+++ flake.nix
@@ -2,24 +2,18 @@
 {
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
-    nixpkgs-unstable.url = "github:nixos/nixpkgs/nixos-unstable";
     # Home Manager
     home-manager = {
       url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
     };
   };
-  # removed nixpkgs-unstable
-  outputs = { self, nixpkgs, nixpkgs-unstable, home-manager, ... }:
+
+  outputs = { self, nixpkgs, home-manager, ... }:
     let
       system = "x86_64-linux";
 
       pkgs = import nixpkgs {
-        inherit system;
-        config.allowUnfree = true;
-      };
-
-      pkgs-unstable = import nixpkgs-unstable {
         inherit system;
         config.allowUnfree = true;
       };
@@ -28,17 +22,13 @@
         inherit system;
         modules = [
           ./configuration.nix
-          ({ ... }: {
-            _module.args = { inherit pkgs-unstable; };
-          })
-
           home-manager.nixosModules.home-manager {
             home-manager.useGlobalPkgs = true;
             home-manager.useUserPackages = true;
-            home-manager.extraSpecialArgs = { inherit pkgs-unstable; };
             home-manager.users.eric = import ./home-manager/home.nix;
           }
         ];
       };
     };
 }
+
